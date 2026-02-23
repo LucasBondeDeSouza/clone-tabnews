d6fcff0836a3ec92056cc560034d0fa867c694e6
Lucas_Bonde <111398229+LucasBondeDeSouza@users.noreply.github.com>
2026-02-23 21:02:18 +0000

    feat: require `read:session` to access `/user` endpoint

--- pages/api/v1/user/index.js
+++ pages/api/v1/user/index.js
@@ -5,7 +5,8 @@ import session from "models/session";
 
 const router = createRouter();
 
-router.get(getHandler);
+router.get(controller.injectAnonymousOrUser);
+router.get(controller.canRequest("read:session"), getHandler);
 
 export default router.handler(controller.errorHandlers);
 
